733107dc0a01a9fcc72fd6dbbfaccb6061ca2bad
Bilqis <hirosasi01@gmail.com>
2021-02-10 23:27:26 +0700

    update main and add calming sound

---
+++ app/src/main/java/com/muflihunnisa/ihealthapplication/CalmingSoundsActivity.kt
@@ -0,0 +1,11 @@
+package com.muflihunnisa.ihealthapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalmingSoundsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calming_sounds)
+    }
+}
